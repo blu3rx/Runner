82f6c30a7243407393a6025cfe1b7bf101415dfc
Salih Gumus <bluesoul.cloud@gmail.com>
2019-06-12 02:05:09 +0300

    x

--- Runner/Assets/Scripts/cameraEngine.cs
+++ Runner/Assets/Scripts/cameraEngine.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class cameraEngine : MonoBehaviour
 {
     private Transform lookAt;
+
     private Vector3 startOffset;
+    private Vector3 moveVector;
+    private Vector3 animationOffset=new Vector3(0,0.5f,0.5f);
+
+    private float transition = 0f;
+    private float animationDuration = 2f;
+    
 
     void Start()
     {
@@ -16,6 +23,24 @@ public class cameraEngine : MonoBehaviour
 
     void Update()
     {
-        transform.position = lookAt.position+startOffset;
+        moveVector = lookAt.position + startOffset;
+
+        //X
+        moveVector.x = 0;
+        //Y
+        moveVector.y = Mathf.Clamp(moveVector.y, .3f, 5);
+
+        if (transition > 1f)
+        {
+            transform.position = moveVector;
+
+        }
+        else
+        {
+            //Animation at the start of the game
+            transform.position = Vector3.Lerp(moveVector + animationOffset, moveVector, transition);
+            transition += Time.deltaTime * 1 / animationDuration;
+        }
+
     }
 }
